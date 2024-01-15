479c3ac780d9d0d80495425c15441f09877c9b2f
Liam White <liamawhite@gmail.com>
2024-01-15 10:53:06 -0800

    fix neovim terminals

--- modules/shared/files/nvim/lua/plugins/terminal.lua
+++ modules/shared/files/nvim/lua/plugins/terminal.lua
@@ -4,24 +4,40 @@ return {
         version = "*",
         config = function()
             require('toggleterm').setup()
-            -- <C-\\><C-n>i ensures we are always in terminal mode when toggling (not just on initial load)
-            vim.keymap.set("n", "<leader>tt", ":ToggleTerm direction=float<CR><C-\\><C-n>i", { noremap = true, silent = true })
-            -- Hide terminal with either <leader>tt or esc
-            vim.keymap.set("t", "<leader>tt", "<C-\\><C-n>:ToggleTerm<CR>", { silent = true })
-            vim.keymap.set("t", "<esc>", "<C-\\><C-n>:ToggleTerm<CR>", { silent = true })
-
-            -- https://github.com/akinsho/toggleterm.nvim#custom-terminal-usage
             local Terminal = require('toggleterm.terminal').Terminal
-            local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float", 
-                    on_close = function()
-                        require("neo-tree.events").fire_event("git_event")
-                    end
+
+            -- Standard Terminal, its created manually so we can close them both using escape
+            local terminal = Terminal:new({ hidden = true, direction = "float" })
+            function _terminal_toggle()
+                terminal:toggle()
+            end
+            vim.keymap.set("n", "<leader>tt", "<cmd>lua _terminal_toggle()<CR><C-\\><C-n>i",
+                 { noremap = true, silent = true })
+            vim.keymap.set("t", "<leader>tt", "<C-\\><C-n><cmd>lua _terminal_toggle()<CR>",
+                 { noremap = true, silent = true })
+
+            -- Lazy git
+            -- https://github.com/akinsho/toggleterm.nvim#custom-terminal-usage
+            local lazygit = Terminal:new({
+                cmd = "lazygit",
+                hidden = true,
+                direction = "float",
+                on_close = function()
+                    require("neo-tree.events").fire_event("git_event")
+                end
             })
             function _lazygit_toggle()
                 lazygit:toggle()
             end
-            vim.keymap.set("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>" ,{noremap = true,silent = true,})
+            vim.keymap.set("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR><C-\\><C-n>i", { noremap = true, silent = true, })
+            vim.keymap.set("t", "<leader>gg", "<C-\\><C-n><cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
 
+            -- Hide all terminal with esc
+            function _all_close()
+                lazygit:close()
+                terminal:close()
+            end
+            vim.keymap.set("t", "<esc>", "<C-\\><C-n><cmd>lua _all_close()<CR>", { silent = true })
         end,
         opts = {}
     },
