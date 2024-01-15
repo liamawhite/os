38a5509241b12d594a804691f43329574cd933fd
Liam White <liamawhite@gmail.com>
2024-01-15 13:33:58 -0800

    Add git reset and fix switch

--- modules/shared/files/nvim/lua/plugins/git.lua
+++ modules/shared/files/nvim/lua/plugins/git.lua
@@ -2,6 +2,11 @@ return {
     {
         -- https://github.com/tpope/vim-fugitive/blob/master/doc/fugitive.txt
         'tpope/vim-fugitive',
+        config = function()
+            -- Reset to upstream master/main
+            vim.keymap.set("n", "<leader>GR", ":Git fetch upstream<CR>:Git checkout master<CR>:Git reset --hard upstream/master<CR>:Git push origin master --force<CR>")
+            vim.keymap.set("n", "<leader>gr", ":Git fetch upstream<CR>:Git checkout main<CR>:Git reset --hard upstream/main<CR>:Git push origin main --force<CR>")
+        end
     },
     {
         'tpope/vim-rhubarb',
