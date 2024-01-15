7ff92f236c8e456237991fd7376df5ceb2e0dfee
Liam White <liamawhite@gmail.com>
2024-01-15 10:19:45 -0800

    remove arc

--- modules/darwin/casks.nix
+++ modules/darwin/casks.nix
@@ -7,14 +7,11 @@ _:
   # Consumer Applications
   "elgato-stream-deck"
   "firefox" # for some reason the pkg doesn't work on macOS
+  "google-chrome"
 
   # Security
   # 1password gui is available in nixpkgs but it has to be ran from /Applications
   "1password"
-
-  # Browsers
-  "google-chrome"
-  "arc"
 
   # AI
   "diffusionbee"
