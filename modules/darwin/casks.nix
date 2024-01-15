be18b046742ae39132990b6000410bde51edda69
Liam White <liamawhite@gmail.com>
2024-01-15 04:53:55 -0800

    Add streamdeck and switch

--- modules/darwin/casks.nix
+++ modules/darwin/casks.nix
@@ -3,6 +3,9 @@ _:
 [
   # System Tools
   "amethyst"
+  
+  # Consumer Applications
+  "elgato-stream-deck"
 
   # Security
   # 1password gui is available in nixpkgs but it has to be ran from /Applications
