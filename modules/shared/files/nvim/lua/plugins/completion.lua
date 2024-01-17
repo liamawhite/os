8231ae8f2280a1cf2903f67b539d2c81ec80814c
Liam White <liamawhite@gmail.com>
2024-01-17 05:26:03 -0800

    Add trouble to vim

--- modules/shared/files/nvim/lua/plugins/completion.lua
+++ modules/shared/files/nvim/lua/plugins/completion.lua
@@ -44,4 +44,20 @@ return {
             })
         end,
     },
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local trouble = require("trouble")
+            trouble.setup({
+                auto_close = true,
+                use_diagnostic_signs = true,
+            })
+            vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end)
+            vim.keymap.set("n", "<leader>xw", function() trouble.toggle("workspace_diagnostics") end)
+            vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end)
+            vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end)
+            vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end)
+        end,
+    },
 }
