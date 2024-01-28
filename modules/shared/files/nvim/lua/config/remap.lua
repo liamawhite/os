cb7f42292dcc5ed61d30fa14b043f786584c95cc
Liam White <liamawhite@gmail.com>
2024-01-28 04:25:14 -0800

    line shifting + yank shortcuts

--- modules/shared/files/nvim/lua/config/remap.lua
+++ modules/shared/files/nvim/lua/config/remap.lua
@@ -17,3 +17,12 @@ vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
 vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+
+-- Yank shortcuts
+vim.keymap.set("n", "<leader>y", '"*y')
+vim.keymap.set("v", "<leader>y", '"*y')
+vim.keymap.set("n", "<leader>Y", 'gg"*yG')
+
+-- Line shifting
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
