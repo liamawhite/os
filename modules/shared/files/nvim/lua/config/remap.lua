ca193d56d3f4f8d1f300bc92217e38114355d060
Liam White <liamawhite@gmail.com>
2024-02-09 05:38:11 -0800

    chmod current file remap

--- modules/shared/files/nvim/lua/config/remap.lua
+++ modules/shared/files/nvim/lua/config/remap.lua
@@ -26,3 +26,5 @@ vim.keymap.set("n", "<leader>Y", 'gg"*yG')
 -- Line shifting
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>x", ":!chmod +x %<CR>") -- Make current file executable
