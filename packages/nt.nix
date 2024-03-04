c1369e8ef6129ead91b431ba60049fb1c2f85b97
Liam White <liamawhite@gmail.com>
2024-03-04 01:42:31 -0800

    bump nt version

--- packages/nt.nix
+++ packages/nt.nix
@@ -4,13 +4,13 @@ with pkgs;
 # https://nixos.org/manual/nixpkgs/stable/#sec-language-go
 buildGoModule rec {
   pname = "nt";
-  version = "v0.0.1";
+  version = "v0.0.2";
 
   src = fetchFromGitHub {
     owner = "liamawhite";
     repo = "nt";
     rev = "${version}";
-    sha256 = "sha256-KNr3PJUc0A0InaTAFOJeKVexHkvzI4ss8xpNGho1AJ8=";
+    sha256 = "sha256-ajUDpa/czDI1xMU7DvqZ9ktyC32L/dbLpnTX5YGt7m4=";
   };
 
   vendorHash = "sha256-0yQMrQXAc7MPEXvOYl1INCj1k4HzViFVeMzv16XCK40=";
