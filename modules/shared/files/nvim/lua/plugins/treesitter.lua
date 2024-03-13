8490e6d9a999b5e47577c3d04320a50fefce2168
Liam White <liamawhite@gmail.com>
2024-03-13 14:19:21 -0700

    fix tsx commenting

--- modules/shared/files/nvim/lua/plugins/treesitter.lua
+++ modules/shared/files/nvim/lua/plugins/treesitter.lua
@@ -7,14 +7,28 @@ return {
 
             configs.setup({
                 -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
-                ensure_installed = { "bash", "c", "cpp", "csv", "dockerfile", "gitignore", "go", "gomod", "gosum", "html", "javascript", "json", "lua", "make", "markdown", "markdown_inline", "python", "regex", "rust", "sql", "terraform", "typescript", "vim", "yaml" },
+                ensure_installed = { "bash", "c", "cpp", "csv", "dockerfile", "gitignore", "go", "gomod", "gosum", "html", "javascript", "json", "lua", "make", "markdown", "markdown_inline", "python", "regex", "rust", "sql", "terraform", "tsx", "typescript", "vim", "yaml" },
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },
+                enable_autocmd = false,
             })
         end
     },
     {
         "nvim-treesitter/playground",
+    },
+    {
+        'numToStr/Comment.nvim',
+        lazy = false,
+        dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+        config = function()
+            require('Comment').setup({
+                pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+            })
+        end,
+    },
+    {
+        'JoosepAlviste/nvim-ts-context-commentstring',
     },
 }
