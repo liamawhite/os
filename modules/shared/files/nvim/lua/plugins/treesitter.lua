ebaf2e70dcd29e86bf96a0173a7f541fc25005f6
Liam White <liamawhite@gmail.com>
2024-03-14 01:38:27 -0700

    add gowork to treesitter

--- modules/shared/files/nvim/lua/plugins/treesitter.lua
+++ modules/shared/files/nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
 
             configs.setup({
                 -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
-                ensure_installed = { "bash", "c", "cpp", "csv", "dockerfile", "gitignore", "go", "gomod", "gosum", "html", "javascript", "json", "lua", "make", "markdown", "markdown_inline", "python", "regex", "rust", "sql", "terraform", "tsx", "typescript", "vim", "yaml" },
+                ensure_installed = { "bash", "c", "cpp", "csv", "dockerfile", "gitignore", "go", "gomod", "gosum", "gowork", "html", "javascript", "json", "lua", "make", "markdown", "markdown_inline", "python", "regex", "rust", "sql", "terraform", "tsx", "typescript", "vim", "yaml" },
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },
