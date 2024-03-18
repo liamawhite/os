007563880e33f59e7235524ce73495a2e842f0b6
Liam White <liamawhite@gmail.com>
2024-03-18 11:31:24 -0700

    fix lua LSP for neovim dev

--- modules/shared/files/nvim/lua/plugins/lsp.lua
+++ modules/shared/files/nvim/lua/plugins/lsp.lua
@@ -22,7 +22,6 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
-            local util = require("lspconfig.util")
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
             lspconfig.bashls.setup({ capabilities = capabilities })
@@ -39,7 +38,25 @@ return {
                     },
                 },
             })
-            lspconfig.lua_ls.setup({ capabilities = capabilities })
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            -- Make the server aware of Neovim runtime files
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                        -- Do not send telemetry data containing a randomized but unique identifier
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+            })
             lspconfig.rust_analyzer.setup({ capabilities = capabilities })
             lspconfig.tailwindcss.setup({ capabilities = capabilities })
             lspconfig.tsserver.setup({
