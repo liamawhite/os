e19e3d39b1ff53edc26b217e87cab878b9e4873d
Liam White <liamawhite@gmail.com>
2024-06-23 10:25:54 -0700

    Markdown preview

--- modules/shared/files/nvim/lua/plugins/lsp_extensions.lua
+++ modules/shared/files/nvim/lua/plugins/lsp_extensions.lua
@@ -97,5 +97,11 @@ return {
             require("inc_rename").setup()
             vim.keymap.set("n", "<leader>rn", ":IncRename ")
         end,
-    }
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
 }
