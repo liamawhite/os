a935bad30e8762250920a997cf48ba857c3b00f3
liam <liamawhite@gmail.com>
2024-11-17 06:27:43 -0800

    turn off go placeholders

--- modules/shared/files/nvim/lua/plugins/lsp.lua
+++ modules/shared/files/nvim/lua/plugins/lsp.lua
@@ -17,7 +17,6 @@ return {
                 settings = {
                     gopls = {
                         completeUnimported = true,
-                        usePlaceholders = true,
                     },
                 },
             })
