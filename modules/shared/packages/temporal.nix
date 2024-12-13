e2cb9452e4987eb9adcf4bebe6550d435eedd5bc
liam <liamawhite@gmail.com>
2024-12-13 04:36:47 -0800

    bump temporal version

--- modules/shared/packages/temporal.nix
+++ modules/shared/packages/temporal.nix
@@ -4,17 +4,17 @@ with pkgs;
 # https://nixos.org/manual/nixpkgs/stable/#sec-language-go
 buildGoModule rec {
   pname = "temporal";
-  version = "1.1.1";
+  version = "1.1.2";
 
   src = fetchFromGitHub {
     owner = "temporalio";
     repo = "cli";
     rev = "v${version}";
-    sha256 = "sha256-pFVCy6xB7Fhj4OatyNQdjkDpDGtod2nJsg2vdl5ED9s=";
+    sha256 = "sha256-7wURMdi357w5S4s909PTUZanRzFyWM588DMY7iYWP88=";
   };
 
-  vendorHash = "sha256-VzY7m5HIsqH3GkHw1YFFL/zAOyR6Hq9MgxlIilU83FM=";
-  ldFlags = "-w -s -X github.com/temporalio/cli/headers.Version=${version}";
+  vendorHash = "sha256-ifXNp3xYHrl/cQgSYi/epdx8NH1W1FBSCq6iuD0dQ/o=";
+  ldFlags = "-s -w -X github.com/temporalio/cli/temporalcli.Version=${version}";
   subPackages = [ "cmd/temporal" ];
 
   meta = with stdenv.lib; {
