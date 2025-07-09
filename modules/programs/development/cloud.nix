536d7b5497fcddf312d1fbaf0474ee4c8469c0b0
Liam White <liamawhite@gmail.com>
2025-07-09 07:05:32 -0700

    add forti

--- modules/programs/development/cloud.nix
+++ modules/programs/development/cloud.nix
@@ -1,5 +1,36 @@
 { user, pkgs, ... }:
 
+let
+  fortio = pkgs.buildGoModule rec {
+    pname = "fortio";
+    version = "1.69.5";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "fortio";
+      repo = "fortio";
+      rev = "v${version}";
+      sha256 = "sha256-Z1BJhWjCJtTOOhXbia56PjkvJbom4ofQVdmxYts3AyU=";
+    };
+
+    vendorHash = "sha256-A8wAfM6GN4NUVuZFhadiaaJPinPRHCjs2L+CRflwqFI=";
+
+    ldflags = [ 
+      "-s" 
+      "-w" 
+      "-X fortio.org/fortio/version.shortVersion=${version}"
+    ];
+
+    # Tests hang due to server lifecycle issues in CLI tests
+    doCheck = false;
+
+    meta = with pkgs.lib; {
+      description = "Load testing library, command line tool, advanced echo server and web UI";
+      homepage = "https://github.com/fortio/fortio";
+      license = licenses.asl20;
+      maintainers = [ ];
+    };
+  };
+in
 {
   home-manager.users.${user} = { ... }: {
     home.packages = with pkgs; [
@@ -8,6 +39,7 @@
       cmctl
       docker-compose
       eksctl
+      fortio
       fluxcd
       google-cloud-sdk
       kubernetes-helm
