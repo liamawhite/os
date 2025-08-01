eaa14f0387589d14fa5927f81e0fd79446e78b70
Liam White <liamawhite@gmail.com>
2025-08-01 14:10:00 -0700

    add device to syncthing

--- modules/programs/syncthing/default.nix
+++ modules/programs/syncthing/default.nix
@@ -13,6 +13,9 @@
           "macbookpro-personal-2018" = {
             id = "OIDLC3D-EX3JH4E-FKEE2X5-YKGVYM4-7QLPPJQ-6ELD5T6-2GQB473-N6CRSAA";
           };
+          "macbookpro-docusign-2025" = {
+            id = "VNBUOSV-NBA7BXM-FCQ2KWS-5YIJNTQ-ZXP2LI6-6HL5ESK-UZY7RRG-7BTW3AU";
+          };
         };
 
         folders = {
