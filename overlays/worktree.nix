5f46b1b5c71d07bfaec3076d6e0466eb743f99f3
Liam White <liamawhite@gmail.com>
2025-08-12 14:06:59 -0700

    bump worktree version

--- overlays/worktree.nix
+++ overlays/worktree.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "worktree";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = fetchFromGitHub {
     owner = "liamawhite";
     repo = "worktree";
     rev = "v${version}";
-    sha256 = "sha256-zArcqiz2DEHQP1X6YvrlK6KGC+smM/pM8/7vUgoKIQg=";
+    sha256 = "sha256-FGNuIauWDNSHtU3SFgZcDbHr5HoBUHlSem5iAQje6P8=";
   };
 
   vendorHash = "sha256-E240usBl6kFhLXlo8ktgdW3PMtP/Qg2yz9n0Ctbkas8=";
