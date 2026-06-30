4c3f4c02e64060ed8dde25bde4dfe202a059c991
Liam White <liamawhite@gmail.com>
2026-06-30 14:24:28 -0700

    update ds autoclone

--- modules/dotfiles/workstreams/types/docusign/onInitAsync.sh
+++ modules/dotfiles/workstreams/types/docusign/onInitAsync.sh
@@ -3,3 +3,32 @@
 # The workstreams command returns immediately; this script outlives it.
 # stdout/stderr are written to .async-init.log inside the workstream directory.
 # Environment variables: WORKSTREAM_DIR, WORKSTREAM_NAME, WORKSTREAM_TYPE
+
+set -e
+
+repos=(
+  "git@github.docusignhq.com:Microservices/network-edge-knowledgebase.git"
+  "git@github.docusignhq.com:Microservices/ds-istio-wasm.git"
+  "git@github.docusignhq.com:Microservices/ds-microservice-shared-chart.git"
+  "git@github.docusignhq.com:Microservices/ds-microservice-simple-chart.git"
+  "git@github.docusignhq.com:Microservices/msf-tools.git"
+  "git@github.docusignhq.com:traffic-mgmt/network-edge-msf-alerting.git"
+  "git@github.docusignhq.com:Microservices/terraform-platform-provisioning.git"
+  "git@github.docusignhq.com:Microservices/terraform-system-compute-provisioning.git"
+  "git@github.docusignhq.com:Microservices/topology.git"
+)
+
+pids=()
+for repo in "${repos[@]}"; do
+  git clone "$repo" "$WORKSTREAM_DIR/$(basename "$repo" .git)" &
+  pids+=($!)
+done
+
+failed=0
+for pid in "${pids[@]}"; do
+  if ! wait "$pid"; then
+    failed=1
+  fi
+done
+
+exit $failed
